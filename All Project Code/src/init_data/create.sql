e5ba4a784d8e8b21aed4c00279a9038798726f98
Regan Wilson <regan.wilson-1@colorado.edu>
2023-12-05 14:26:52 -0700

    fixed the SQL issue, but bcrypt is acting up

--- All Project Code/src/init_data/create.sql
+++ All Project Code/src/init_data/create.sql
@@ -4,15 +4,19 @@ CREATE TABLE users(
     password CHAR(60) NOT NULL
 );
 
-CREATE TABLE users_to_themes(
-    username VARCHAR(50) FOREIGN KEY,
-    avatar INT FOREIGN KEY
-);
-
 CREATE TABLE themes(
    avatar INT PRIMARY KEY,
    theme INT
 );
+
+CREATE TABLE users_to_themes(
+    username VARCHAR(50),
+    avatar INT,
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (avatar) REFERENCES themes (avatar)
+);
+
+
 
 -- CREATE TABLE users_to_themes(
 --     username VARCHAR(50) REFERENCES users(username),
--- All Project Code/src/init_data/insert.sql
+++ All Project Code/src/init_data/insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO users (username, email, password) values ('JDoe','JohnDoe@gmail.com','$2b$10$10B01VBxolqTy906W1FnuuGYcGutxhQh8XLpMU.UC2vIGmYMFatzm');
+INSERT INTO themes (avatar, theme) VALUES (1, 1);
 INSERT INTO users_to_themes (username, avatar) VALUES ('JDoe', 1);
 -- Password for JDoe account is 'qwerty'INSERT INTO users_to_themes (username, avatar) VALUES ('JDoe', 1);
