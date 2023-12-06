bb626bc12577113abe2b30ea996f4ce7fb94cb9a
Colin <coca4266@colorado.edu>
2023-12-06 16:04:01 -0700

    added functionality for the avatars

--- All Project Code/src/init_data/create.sql
+++ All Project Code/src/init_data/create.sql
@@ -1,22 +1,17 @@
-CREATE TABLE users(
+CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     email VARCHAR(50) NOT NULL,
     password CHAR(60) NOT NULL
 );
 
-CREATE TABLE themes(
+CREATE TABLE themes (
    avatar INT PRIMARY KEY,
    theme INT
 );
 
-CREATE TABLE users_to_themes(
+CREATE TABLE users_to_themes (
     username VARCHAR(50) REFERENCES users(username),
-    email VARCHAR(50) NOT NULL,
-    password CHAR(60) NOT NULL
-    avatar INT REFERENCES themes(avatar)
-);
-
-CREATE TABLE themes(
-   avatar INT PRIMARY KEY,
-   theme INT
+    avatar INT REFERENCES themes(avatar),
+    PRIMARY KEY (username, avatar),
+    UNIQUE (username)
 );
